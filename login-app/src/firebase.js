ad5ab2d31d663ae2dd401c0014092de361dd9c92
Thomas Fabri <100315570+ThomasFabri@users.noreply.github.com>
2025-05-01 14:43:00 -0300

    Update firebase.js

--- login-app/src/firebase.js
+++ login-app/src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCBiXUzJ4cHlgJSwlRl-jxM0zGkyTIWZYU",
-    authDomain: "react-app-d06ce.firebaseapp.com",
-    projectId: "react-app-d06ce",
-    storageBucket: "react-app-d06ce.firebasestorage.app",
-    messagingSenderId: "1065525312262",
-    appId: "1:1065525312262:web:2d6288ab13ca501481bc87"
+    apiKey: "***",
+    authDomain: "***",
+    projectId: "***",
+    storageBucket: "***",
+    messagingSenderId: "***",
+    appId: "***"
   };
 
 const app = initializeApp(firebaseConfig);
